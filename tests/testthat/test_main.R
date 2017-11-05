6bc8e9c9d8018012d4b39eeeb9dab9cc54e9a8fc
Anthony <anthonyebert@gmail.com>
2017-11-05 23:42:10 +1000

    Remove kernlab dependency.

--- tests/testthat/test_main.R
+++ tests/testthat/test_main.R
@@ -2,15 +2,15 @@
 library(testthat)
 library(EasyMMD)
 
-x <- rnorm(4000)
-y <- rnorm(4500, 5)
+x <- c(rnorm(2000), rnorm(2000, 5))
+y <- c(rnorm(2500, 5), rnorm(2500, 20))
 
-test_that("MMD calc", {
-  expect_equal(MMD(x,y, threshold = 6), EasyMMD:::MMD_oldR(x,y))
-})
 
-print(MMD(x,y))
 
 print("C++ version speed. Approx ~ 0.84 on the Debian machine")
+print(MMD(x,y))
 print(system.time(MMD(x,y)))
 
+print("C++ version speed with threshold = 6. Approx ~ 0.84 on the Debian machine")
+print(MMD(x,y, threshold = 6))
+print(system.time(MMD(x,y, threshold = 6)))
